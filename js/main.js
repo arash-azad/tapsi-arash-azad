89d8050bd29598561d210fd87dd2b4bafa9ad47c
arash <arash.azad.2006@gmail.com>
2025-07-17 10:01:41 +0330

    01-justForFun
    For fun

---
+++ js/main.js
@@ -0,0 +1,27 @@
+let btn1=document.getElementById("service1")
+let btn2=document.getElementById("service2")
+let btn3=document.getElementById("service3")
+let btn4=document.getElementById("service4")
+let btn5=document.getElementById("service5")
+let btn6=document.getElementById("service6")
+let frame=document.getElementById("imginit")
+
+
+btn1.addEventListener("click", function(){
+  frame.className = "box21";
+})
+btn2.addEventListener("click", function(){
+  frame.className = "box22";
+})
+btn3.addEventListener("click", function(){
+  frame.className = "box23";
+})
+btn4.addEventListener("click", function(){
+  frame.className = "box24";
+})
+btn5.addEventListener("click", function(){
+  frame.className = "box25";
+})
+btn6.addEventListener("click", function(){
+  frame.className = "box26";
+})
